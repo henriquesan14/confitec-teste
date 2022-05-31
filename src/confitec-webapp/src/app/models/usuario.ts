e97254f795e590a4a22cb79f5dba5ecd2586c142
José Henrique da Silva Santos <jose.labsit@dotzmkt.com.br>
2022-05-31 10:54:33 -0300

    adicionado app-angular

---
+++ src/confitec-webapp/src/app/models/usuario.ts
@@ -0,0 +1,10 @@
+export interface Usuario {
+    id: number;
+    nome: string;
+    sobrenome: string;
+    email: string;
+    dataNascimento: string;
+    escolaridade: string;
+    criadoEm: string;
+    atualizadoEm: string;
+}
